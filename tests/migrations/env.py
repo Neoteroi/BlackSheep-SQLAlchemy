3c1759cda92ad0b95ef168695f3bb576616fd4e2
Roberto Prevato <roberto.prevato@gmail.com>
2021-05-22 11:53:14 +0200

    corrects test migrations import

--- tests/migrations/env.py
+++ tests/migrations/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from domain import metadata
+from tests.domain import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
